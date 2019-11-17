d61073f9f4d1c767f45d2578aae4029285647c1f
wbt5 <wubangtu@qq.com>
2019-11-18 04:12:36 +0800

    新增快手直播

---
+++ kuaishou.py
@@ -0,0 +1,26 @@
+# 获取快手直播的真实流媒体地址。
+# 输出是最高画质
+
+
+import requests
+import re
+
+
+def get_real_url(url):
+    try:
+        room_url = 'https://m.gifshow.com/fw/live/' + \
+            re.findall(r'[u|live|profile]/([\w-]*)[\s\S]*?', url)[0]
+        headers = {
+            'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1'}
+        response = requests.get(url=room_url, headers=headers).text
+        m3u8_url = re.findall(
+            r'type="video/mp4" src="([\s\S]*?.m3u8)', response)[0]
+        real_url = re.sub(r'_sd1000', '', m3u8_url)
+    except:
+        real_url = '直播间不存在或未开播'
+    return real_url
+
+
+rid = input('请输入快手直播间地址：\n')
+real_url = get_real_url(rid)
+print('该直播源地址为：\n' + real_url)
