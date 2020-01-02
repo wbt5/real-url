fdfce61e9d208e4df262f73659762cb4457dcf7f
wbt5 <wubangtu@qq.com>
2020-01-03 00:09:58 +0800

    修复快手直播

--- kuaishou.py
+++ kuaishou.py
@@ -6,21 +6,20 @@ import requests
 import re
 
 
-def get_real_url(url):
+def get_real_url(rid):
     try:
-        room_url = 'https://m.gifshow.com/fw/live/' + \
-            re.findall(r'[u|live|profile]/([\w-]*)[\s\S]*?', url)[0]
+        room_url = 'https://m.gifshow.com/fw/live/' + str(rid)
         headers = {
             'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1'}
         response = requests.get(url=room_url, headers=headers).text
-        m3u8_url = re.findall(
-            r'type="video/mp4" src="([\s\S]*?.m3u8)', response)[0]
-        real_url = re.sub(r'_sd1000', '', m3u8_url)
+        m3u8_url = re.findall(r'type="video/mp4" src="([\s\S]*?)_sd1000tp.m3u8', response)[0]
+        real_url = [m3u8_url + i for i in ['.flv', '.m3u8']]
     except:
         real_url = '直播间不存在或未开播'
     return real_url
 
 
-rid = input('请输入快手直播间地址：\n')
+rid = input('请输入快手直播间ID：\n')
 real_url = get_real_url(rid)
-print('该直播源地址为：\n' + real_url)
+print('该直播源地址为：')
+print(real_url)
