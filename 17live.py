6b290826dce7c825959caee648ac9c1ef57d0dfd
wbt5 <wubangtu@qq.com>
2020-05-23 02:55:37 +0800

    更新17直播

--- 17live.py
+++ 17live.py
@@ -6,8 +6,13 @@ import requests
 
 def get_real_url(rid):
     try:
-        response = requests.post(url='https://api-dsa.17app.co/api/v1/lives/' + rid +'/viewers/alive', data='{"liveStreamID":rid}').json()
-        real_url = response.get('rtmpUrls')[0].get('url')
+        response = requests.get(url='https://api-dsa.17app.co/api/v1/lives/' + rid).json()
+        real_url_default = response.get('rtmpUrls')[0].get('url')
+        userID = response.get('userID', 0)
+        real_url_wansu = ''
+        if userID:
+            real_url_wansu = 'http://wansu-china-pull-rtmp-17.tigafocus.com/vod/' + userID + '.flv'
+        real_url = [real_url_default, real_url_wansu]
     except:
         real_url = '该直播间不存在或未开播' 
     return real_url
