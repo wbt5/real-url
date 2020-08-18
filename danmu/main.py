40eecd5bcf85025bdcfefafb7f16653f6fe78af7
wbt5 <wubangtu@qq.com>
2020-08-18 21:53:10 +0800

    新增2个平台

--- danmu/main.py
+++ danmu/main.py
@@ -23,19 +23,21 @@ async def main(url):
 a = input('请输入直播间地址：\n')
 asyncio.run(main(a))
 
-# 虎牙：https://www.huya.com/11352915
-# 斗鱼：https://www.douyu.com/85894
-# B站：https://live.bilibili.com/70155
-# 快手：https://live.kuaishou.com/u/jjworld126
-# 火猫：
+# 虎牙直播：https://www.huya.com/11352915
+# 斗鱼直播：https://www.douyu.com/85894
+# B站直播：https://live.bilibili.com/70155
+# 快手直播：https://live.kuaishou.com/u/jjworld126
+# 火猫直播：
 # 企鹅电竞：https://egame.qq.com/383204988
 # 花椒直播：https://www.huajiao.com/l/303344861?qd=hu
 # 映客直播：https://www.inke.cn/liveroom/index.html?uid=87493223&id=1593906372018299
 # CC直播：https://cc.163.com/363936598/
 # 酷狗直播：https://fanxing.kugou.com/1676290
-# 战旗直播
+# 战旗直播：
 # 龙珠直播：http://star.longzhu.com/wsde135864219
 # PPS奇秀直播：https://x.pps.tv/room/208337
 # 搜狐千帆直播：https://qf.56.com/520208a
 # 来疯直播：https://v.laifeng.com/656428
 # LOOK直播：https://look.163.com/live?id=196257915
+# AcFun直播：https://live.acfun.cn/live/23682490
+# 艺气山直播：http://www.173.com/96
