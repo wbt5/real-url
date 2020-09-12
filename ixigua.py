6ec73b61d954755b430b451cd43c3dece61784a2
wbt5 <wubangtu@qq.com>
2020-09-12 09:41:25 +0800

    update

--- ixigua.py
+++ ixigua.py
@@ -8,12 +8,9 @@ import json
 def get_real_url(rid):
     try:
         room_url = rid
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0'
-        }
-        response = requests.get(url=room_url, headers=headers).text
+        response = requests.get(url=room_url).text
         real_url = re.findall(r'playInfo":([\s\S]*?),"authStatus', response)[0]
-#        real_url = re.sub(r'\\u002F', '/', real_url)
+        real_url = re.sub(r'\\u002F', '/', real_url)
     except:
         real_url = '直播间不存在或未开播'
     return real_url
