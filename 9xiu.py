009574e91848c34908b4d3f2613de7d42d67b69a
wbt5 <wubangtu@qq.com>
2021-11-07 20:46:06 +0800

    :zap: 优化代码

--- 9xiu.py
+++ 9xiu.py
@@ -10,7 +10,7 @@ class JXiu:
 
     def get_real_url(self):
         with requests.Session() as s:
-            url = 'https://h5.9xiu.com/room/live/enterRoom?rid=' + str( self.rid)
+            url = f'https://h5.9xiu.com/room/live/enterRoom?rid={self.rid}'
             headers = {
                 'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) '
                               'AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1'
