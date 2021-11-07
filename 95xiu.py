f26b0f4c035ab138cb1495debbad387da5fcc5e2
wbt5 <wubangtu@qq.com>
2021-11-07 20:38:55 +0800

    :zap: 优化代码

--- 95xiu.py
+++ 95xiu.py
@@ -11,14 +11,14 @@ class JWXiu:
 
     def get_real_url(self):
         with requests.Session() as s:
-            res = s.get('http://www.95.cn/{}.html'.format(self.rid)).text
+            res = s.get(f'https://www.95.cn/{self.rid}.html').text
         try:
             uid = re.search(r'"uid":(\d+),', res).group(1)
             status = re.search(r'"is_offline":"(\d)"', res).group(1)
         except AttributeError:
             raise Exception('没有找到直播间')
         if status == '0':
-            real_url = 'http://play.95xiu.com/app/{}.flv'.format(uid)
+            real_url = f'https://play1.95xiu.com/app/{uid}.flv'
             return real_url
         else:
             raise Exception('未开播')
@@ -36,4 +36,3 @@ def get_real_url(rid):
 if __name__ == '__main__':
     r = input('输入95秀房间号：\n')
     print(get_real_url(r))
-
